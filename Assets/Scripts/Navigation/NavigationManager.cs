aa8817b45d7f1c80e8b90e01ceaef7207d125f55
jay <phamducdat14122000@gmail.com>
2024-12-14 09:40:00 +0700

    feat/the_world_map

---
+++ Assets/Scripts/Navigation/NavigationManager.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public struct Route {
+    public string RouteDescription;
+    public bool CanTravel;
+}
+
+public static class NavigationManager {
+    public static Dictionary<string, Route> RouteInformation = new
+      Dictionary<string, Route>() {
+           { "Overworld", new Route {
+       RouteDescription = "The big bad world", CanTravel = true}},
+        { "Construction", new Route {
+       RouteDescription = "The construction area", CanTravel = false}},
+     {"Town", new Route {
+       RouteDescription = "The main town", CanTravel = true}},
+     { "Campsite", new Route {
+       RouteDescription = "The campsite",
+         CanTravel = false}},
+       };
+
+    public static string GetRouteInfo(string destination) {
+        return RouteInformation.ContainsKey(destination) ? RouteInformation[destination].RouteDescription : null;
+    }
+
+    public static bool CanNavigate(string destination) {
+        return RouteInformation.ContainsKey(destination) ? RouteInformation[destination].CanTravel : false;
+    }
+
+    public static void NavigateTo(string destination) {
+        SceneManager.LoadScene(destination);
+    }
+}
--- Assets/Scripts/NavigationPrompt.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-public class NavigationPrompt : MonoBehaviour {
-    void OnCollisionEnter2D(Collision2D col) {
-        if (col.gameObject.CompareTag("Borders")) {
-            Debug.Log("leave town");
-        }
-    }
-}
